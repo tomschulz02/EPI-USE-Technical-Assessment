4097d69090bfd59aa41cd3020059836578b695b8
Tom Schulz <91066892+tomschulz02@users.noreply.github.com>
2025-08-13 16:00:34 +0200

    Repository layer functions created

---
+++ backend/src/repositories/employeesRepository.js
@@ -0,0 +1,70 @@
+import { error } from 'console';
+import pool from '../db';
+
+async function fetchAllEmployees() {
+	try {
+		const result = await pool.query('SELECT * FROM employees;');
+		return result.rows;
+	} catch (err) {
+		console.error(err);
+		return 'DB_ERROR';
+	}
+}
+
+async function fetchEmployee(id) {
+	try {
+		const result = await pool.query('SELECT * FROM employees WHERE id=$1;', [id]);
+		return result.rows[0];
+	} catch (err) {
+		console.error(err);
+		return 'DB_ERROR';
+	}
+}
+
+async function createEmployee(name, surname, dob, emp_no, salary, role, email, manager = null) {
+	try {
+		const result = await pool.query(
+			'INSERT INTO employees (name, surname, birth_date, employee_number, salary, role, manager_id, email) VALUES ($1, $2, $3, $4, $5, $6, $7, $8);',
+			[name, surname, dob, emp_no, salary, role, manager, email]
+		);
+		return result.rowCount;
+	} catch (err) {
+		console.error(err);
+		return 'DB_ERROR';
+	}
+}
+
+async function updateEmployee(id, name, surname, dob, emp_no, salary, role, email, manager = null) {
+	try {
+		const result = await pool.query(
+			`
+                UPDATE employees
+                SET name=$1, surname=$2, birth_date=$3, employee_number=$4, salary=$5, role=$6, manager_id=$7, email=$8
+                WHERE id=$9;
+            `,
+			[name, surname, dob, emp_no, salary, role, manager, email, id]
+		);
+		return result.rowCount;
+	} catch (err) {
+		console.error(err);
+		return 'DB_ERROR';
+	}
+}
+
+async function deleteEmployee(id) {
+	try {
+		const result = await pool.query('DELETE FROM employees WHERE id=$1 RETURNING *;', [id]);
+		return result.rows[0];
+	} catch (err) {
+		console.error(err);
+		return 'DB_ERROR';
+	}
+}
+
+export const employeesRepo = {
+	fetchAllEmployees,
+	fetchEmployee,
+	createEmployee,
+	updateEmployee,
+	deleteEmployee,
+};
