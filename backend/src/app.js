38eeff7dfeac71b0e349614eaa282d7f374ebce4
Tom Schulz <91066892+tomschulz02@users.noreply.github.com>
2025-08-13 20:27:51 +0200

    Deployment bugfix

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 
-import router from './routes/employees';
+import router from './routes/employees.js';
 
 const app = express();
 app.use(cors());
